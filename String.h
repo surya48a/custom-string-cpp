0f09ae506b3799f3dc5c4f244e841448a2a8d1e9
surya <suryabot48@gmail.com>
2026-07-22 00:53:22 +0530

    Added all cstring functions declarations

--- String.h
+++ String.h
@@ -1,8 +1,8 @@
 #ifndef STRING_H_
 #define STRING_H_
 
-#include<cstring>
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 namespace surya
@@ -21,7 +21,7 @@ namespace surya
 			~STRING();
 
 			// Relational and logical operator overloading declarations
-			STRING operator +(STRING &);
+			STRING& operator +(STRING &);
 			char& operator [](const int) const;
 			//bool operator > (STRING &) const;
 			//bool operator < (STRING &) const;
@@ -34,9 +34,34 @@ namespace surya
 			friend istream& operator >> (istream& , STRING &);
 			friend ostream& operator << (ostream& , STRING &);
 
+			// Extra features
+			char& at(const int) const;
+			char& front() const;
+			char& back() const;
+			unsigned int length() const;
+			unsigned int size() const;
+			bool empty() const;
+			const char * c_str() const;
+			void clear();
+
+// friend functions
+friend void strcpy(STRING & , const STRING &);
+friend void strncpy(STRING& , const STRING &, const int);
+friend int strcmp(const STRING& , const STRING&);
+friend int strncmp(const STRING& ,const STRING& , const int);
+friend void strcat(STRING & , const STRING &);
+friend void strncat(STRING & , const STRING & ,const int);
+friend void strrev(STRING &);
+friend void strupper(STRING &);
+friend void strlower(STRING &);
+friend char * strchr(const STRING& , const char);
+friend char * strrchr(const STRING& , const char);
+friend char * strstr(const STRING& , const char *);
+friend unsigned int strlen(const STRING&);
+
+
 	};
 }
 
-#include "string.cpp"
 
 #endif
